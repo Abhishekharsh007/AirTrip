c5aa74aeecf51cd95767a00687193475efcf2b00
Abhishekharsh007 <abhishekharsh3411@gmail.com>
2026-01-08 17:37:28 +0530

    Hotel List Page is done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,24 +1,25 @@
-import React from 'react';
+import React from "react";
 import Navbar from "./components/Navbar.jsx";
-import { Route, Routes, useLocation } from 'react-router-dom';
-import Home from './pages/Home.jsx';
-import Footer from './components/Footer.jsx';
+import { Route, Routes, useLocation } from "react-router-dom";
+import Home from "./pages/Home.jsx";
+import Footer from "./components/Footer.jsx";
+import AllRooms from "./pages/AllRooms.jsx";
 
 const App = () => {
-
   const isOwnerPath = useLocation().pathname.includes("owner");
 
   return (
     <div>
       {!isOwnerPath && <Navbar />}
-      <div className='min-h-[70vh]'>
+      <div className="min-h-[70vh]">
         <Routes>
-          <Route path='/' element={<Home/>} />
+          <Route path="/" element={<Home />} />
+          <Route path="/rooms" element={<AllRooms />} />
         </Routes>
       </div>
       <Footer />
     </div>
   );
-}
+};
 
-export default App
+export default App;
