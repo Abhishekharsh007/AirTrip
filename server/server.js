0926cc07ea35714a17d9684b22294e4c9bcfe2d0
Abhishekharsh007 <abhishekharsh3411@gmail.com>
2026-01-13 21:26:24 +0530

    Basic of backend setup is done

---
+++ server/server.js
@@ -0,0 +1,12 @@
+import express from "express";
+import "dotenv/config";
+import cors from "cors";
+
+const app = express();
+app.use(cors());  // Enabling Cross-Origin Resource Sharing
+
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => res.send("API is working good and fine"));
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
